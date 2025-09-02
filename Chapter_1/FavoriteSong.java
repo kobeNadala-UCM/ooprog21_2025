a77589ed1aed2cb526e9169497c89922025f6cc9
kobeNadala-UCM <karyllbeyoncejereznadala67@gmail.com>
2025-09-02 16:24:35 +0900

    FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,1 +1,11 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Life goes on");
+        System.out.println("The light ends the dark");
+        System.out.println("Mistakes made forgot");
+        System.out.println("Through years and gods");
+        System.out.println("Forever we are");
+        System.out.println("Infinite");
+    }
+} 
 
