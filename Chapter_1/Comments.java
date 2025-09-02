a8e9f5fa1f21c4555041335ee22479009dde1e7b
kobeNadala-UCM <karyllbeyoncejereznadala67@gmail.com>
2025-09-02 16:59:30 +0900

    Comments.java

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,1 +1,24 @@
+/**
+ * This program demonstrates the three types of comments in Java.
+ */
+public class Comments {
+    public static void main(String[] args) {
+        
+        // Single-line comment
+        System.out.println("Demonstrating three types of comments in Java.");
+
+        /*
+         Multi-line comment
+         Useful for explaining blocks of code.
+        */
+        System.out.println("Multi-line comments can span multiple lines.");
+
+        /**
+         * Documentation comment
+         * Commonly used for generating documentation.
+         */
+        System.out.println("Documentation comments are used for Javadoc.");
+    }
+}
+
 
