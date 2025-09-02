16a9e22b10ec67b0956aa9b93e289670e2135a33
kobeNadala-UCM <karyllbeyoncejereznadala67@gmail.com>
2025-09-03 01:04:06 +0900

    FirstDialog.java

--- Chapter_1/FirstDialog.java
+++ Chapter_1/FirstDialog.java
@@ -2,12 +2,8 @@ import javax.swing.JOptionPane;
 
 public class FirstDialog {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(
-            null, 
-            "First Java dialog", 
-            "Message", 
-            JOptionPane.INFORMATION_MESSAGE
-        );
+
+       JOptionPane.showMessageDialog(null, "First Java dialog");
+
     }
 }
-
