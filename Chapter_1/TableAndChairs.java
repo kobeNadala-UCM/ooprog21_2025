d99721aab28c5a01edbe50616977d30e26303b7a
kobeNadala-UCM <karyllbeyoncejereznadala67@gmail.com>
2025-09-02 16:15:46 +0900

    TableAndChairs.java

--- Chapter_1/TableAndChairs.java
+++ Chapter_1/TableAndChairs.java
@@ -1,1 +1,10 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("*                           *");
+        System.out.println("*                           *");
+        System.out.println("******     *******     ******");
+        System.out.println("*    *     *     *     *    *");
+        System.out.println("*    *     *     *     *    *\n\n");
+    }
+}
 
