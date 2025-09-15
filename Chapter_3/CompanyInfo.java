4b66de623847013b542325ca27428c400bf600e2
kobeNadala-UCM <karyllbeyoncejereznadala67@gmail.com>
2025-09-15 21:31:16 +0900

    CompanyInfo.java

--- Chapter_3/CompanyInfo.java
+++ Chapter_3/CompanyInfo.java
@@ -9,7 +9,7 @@ public class CompanyInfo {
           public static void displayHours() {
              System.out.println("Monday-Friday  :8 a.m - 6 p.m");
              System.out.println("Saturday       :8 a.m - noon");
-             System.out.println("Sunday         : Closed");
+             System.out.println("Sunday         :CLOSED");
              }
  
 
